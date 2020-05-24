e8240422c442bcaeae3c2b554d2a453ea970989c
wangyaoding <wang.yaoding01@iwhalecloud.com>
2020-05-24 18:35:42 +0800

    init

---
+++ src/main/java/com/yaoding/fundamental/sort/Sort.java
@@ -0,0 +1,13 @@
+package com.yaoding.fundamental.sort;
+
+/**
+ *  定义抽象类的排序方法，用于每个排序的算法，都按照此模板
+ */
+public abstract class Sort {
+    //具体的抽象方法
+    public abstract void sort(Comparable[] arr);
+
+
+
+
+}
